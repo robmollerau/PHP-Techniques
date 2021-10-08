660f51ad043e2fccacf828fb9c2aa63f88fd2c54
Robert Moller AU <robmollerau@users.noreply.github.com>
2021-10-08 21:20:21 +1100

    Addition to basicArrays.php

--- basicArrays.php
+++ basicArrays.php
@@ -8,31 +8,44 @@
     /* Force strict types */
     declare( strict_types = 1 );
 
-    // Constants ///////////////////////////////////////////////////////////////
+    // Constant Arrays /////////////////////////////////////////////////////////
 
-    /* Constant arrays - Single dimension */
+    /* Constant array - Single dimension */
     const ANIMALS_ARRAY = array( 'dog', 'cat', 'mouse' );
 
-    /* Constant arrays - Single dimension with key */
+    /* Constant array - Single dimension with key */
     const SIGNIFICANT_YEARS = array(
         1770 => 'Captain Cook east coast exploration',
         1788 => 'Penal colony Sydney cove',
         1808 => 'Rum rebellion',
         1851 => 'Gold rush NSW',
-        1854 => 'Batle of the Eureka Stockade',
+        1854 => 'Battle of the Eureka Stockade',
         1901 => 'Federation'
     );
 
+    /* Country capital array columns */
     const COUNTRY     = 0;
     const CAPITAL     = 1;
     const POPULATION  = 2;
 
-    /* Constant arrays - Two dimension */
+    /* Constant array - Two dimension */
     const COUNTRY_CAPITAL_ARRAY = array(
         array( 'Australia', 'Canberra',   400000 ),
         array( 'France',    'Paris',     2100000 ),
         array( 'Austria',   'Vienna',    1900000 )
     );
+
+    /* Country code array columns */
+    const CC_COUNTRY      = 0;
+    const CC_CAPITAL      = 1;
+    const CC_CALLING_CODE = 2;
+
+    /* Constant array - Two dimension with key */
+    const COUNTRY_CODE_ARRAY = array(
+        'ARG' => array( 'Argentina', 'Buenos Aires', '011' ),
+        'BRA' => array( 'Brazil',    'Brasilia',     '55'  ),
+        'COL' => array( 'Colombia',  'Bogota',       '57'  )
+    );    
 
     // Main ////////////////////////////////////////////////////////////////////
 
@@ -64,7 +77,7 @@
             $Row[ POPULATION ] . PHP_EOL );
     }
 
-    /* Array creation */
+    /* Empty two dimension array */
     $Array1 = array( array() );
 
     /* Set all elements to zero at the same time dimensioning array */
@@ -83,7 +96,55 @@
     /* Print all elements */
     print_r( $Array1 );
 
+    /* Lookup key in two dimension constant array */
+    $Key = 'COL';
+    if ( array_key_exists( $Key, COUNTRY_CODE_ARRAY ) ) {
+
+        /* Key lookup returns the subarray */
+        $LookupArray = COUNTRY_CODE_ARRAY[ $Key ];
+
+        /* Print elements of subarray */
+        echo( 'Country code Found' . PHP_EOL );
+        
+        echo( 'Country Code: ' . $Key . PHP_EOL .
+              'Country:      ' . $LookupArray[ CC_COUNTRY      ] . PHP_EOL .
+              'Capital:      ' . $LookupArray[ CC_CAPITAL      ] . PHP_EOL .
+              'Calling Code: ' . $LookupArray[ CC_CALLING_CODE ] . PHP_EOL );
+
+    } else {
+        echo( 'Key ' . $Key . ' not found' . PHP_EOL );
+    }    
+
+    /* Get first key */
+    echo( 'First key: ' . array_key_first( COUNTRY_CODE_ARRAY ) . PHP_EOL );
+
+    /* Get last key */
+    echo( 'Last Key: ' . array_key_last( COUNTRY_CODE_ARRAY ) . PHP_EOL );
+
+    /* Adding to array - single dimension */
+    $ArrayBirds = [];
+
+    array_push( $ArrayBirds, 'rainbow lorikeet', 'galah', 'bowerbird', 'cockatoo', 'magpie' );
+    print_r( $ArrayBirds );
+
+    /* Remove last element */
+    print_r( array_pop( $ArrayBirds ) );
+
+    /* When creating subarrays we clone the original array sa array_chunk will modify original array */
+    $TempArray = array_merge( $ArrayBirds );
+
+    /* Create arrays from an array - will create two sub arrays */
+    $ArrayBirds2 = array_chunk( $TempArray, 2 );
+    echo( 'Birds 2' . PHP_EOL );
+    //print_r( $ArrayBirds2 );
+
+    /* Sorting birds array */
+    print_r( sort( $ArrayBirds ) );
+
     
+
+
+
 
   
 
